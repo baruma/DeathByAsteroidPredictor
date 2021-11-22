0752c404f4c15f22e0ccd6f15612f44fb8a7f756
Liana Haque <lianahaque@Lianas-Pro.attlocal.net>
2021-11-22 06:47:42 -0800

    Fixed PictureOfDay appearance issue - need to set date/time to current

--- app/src/main/java/com/udacity/asteroidradar/networkLayer/AsteroidService.kt
+++ app/src/main/java/com/udacity/asteroidradar/networkLayer/AsteroidService.kt
@@ -2,6 +2,7 @@ package com.udacity.asteroidradar.networkLayer
 
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import com.udacity.asteroidradar.Asteroid
 import com.udacity.asteroidradar.BuildConfig
 import com.udacity.asteroidradar.PictureOfDay
 import retrofit2.Retrofit
@@ -9,26 +10,24 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
 
-private val BASE_URL = "https://api.nasa.gov/planetary/"
+private val BASE_URL = "https://api.nasa.gov/neo/rest/v1/feed?"
 
 private val moshi = Moshi.Builder()
     .add(KotlinJsonAdapterFactory())
     .build()
 
-val retrofitPictureOfDay = Retrofit.Builder()
+val retrofitAsteroidService: Retrofit = Retrofit.Builder()
     .addConverterFactory(MoshiConverterFactory.create(moshi))
     .baseUrl(BASE_URL)
     .build()
 
-// Got a 403 error code earlier because I had a space between the '=' and Key.
-interface PictureOfDayService {
-    @GET("apod?api_key=${BuildConfig.NASA_KEY}")
-    suspend fun getPictureOfDay(): PictureOfDay
+interface AsteroidService {
+    @GET
+    suspend fun getAsteroids(): List<Asteroid>
 }
 
-object PictureOfDayAPI {
-    val retrofitService: PictureOfDayService by lazy {
-        retrofitPictureOfDay.create(PictureOfDayService::class.java)
+object AsteroidAPI {
+    val retrofitService: AsteroidService by lazy {
+        retrofitPictureOfDay.create(AsteroidService::class.java)
     }
 }
-
